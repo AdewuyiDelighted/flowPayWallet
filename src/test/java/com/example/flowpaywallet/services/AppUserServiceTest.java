68191a56e36d027264fa1667678c97d91630d441
delighted <deborahdelighted5@gmail.com>
2024-07-30 01:20:01 +0100

    wip:worked on user model,wallet model,create user,create wallet

---
+++ src/test/java/com/example/flowpaywallet/services/AppUserServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.flowpaywallet.services;
+
+import com.example.flowpaywallet.Exceptions.UserAlreadyExistException;
+import com.example.flowpaywallet.dto.requests.CreateUserRequest;
+import com.example.flowpaywallet.dto.response.CreateUserResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest
+public class AppUserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testThatAccountUserCanBeCreated() {
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setFullName("First AppUser");
+        createUserRequest.setEmail("firstUser1@gmail.com");
+        CreateUserResponse createUserResponse = userService.createUser(createUserRequest);
+        assertThat(createUserResponse.getMessage()).isNotNull();
+    }
+    @Test public void testThatUserWithExistingEmailCannotBeCreatedAgain(){
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setFullName("First AppUser");
+        createUserRequest.setEmail("firstUser1@gmail.com");
+        assertThrows(UserAlreadyExistException.class,()->userService.createUser(createUserRequest));
+    }
+}
