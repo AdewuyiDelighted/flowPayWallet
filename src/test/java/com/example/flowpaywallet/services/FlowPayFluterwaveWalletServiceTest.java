512f989241b07ecc92bbc74648687d109014d28f
delighted <deborahdelighted5@gmail.com>
2024-08-12 23:35:06 +0100

    FlowPayFluterwaveWalletServiceTest

---
+++ src/test/java/com/example/flowpaywallet/services/FlowPayFluterwaveWalletServiceTest.java
@@ -0,0 +1,9 @@
+package com.example.flowpaywallet.services;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FlowPayFluterwaveWalletServiceTest {
+
+}
