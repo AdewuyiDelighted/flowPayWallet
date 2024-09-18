2bf65917cadfa064b869819016655fa804ca8986
delighted <deborahdelighted5@gmail.com>
2024-09-19 00:50:06 +0100

    Refactored existing code, added new files

--- src/main/java/com/example/flowpaywallet/dto/requests/CreateWalletRequest.java
+++ src/main/java/com/example/flowpaywallet/dto/requests/CreateWalletRequest.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Setter
 @Getter
 public class CreateWalletRequest {
-    private String email;
+    private Long appUserId;
+    private String pin;
 
 }
--- src/main/java/com/example/flowpaywallet/dto/response/CreateUserResponse.java
+++ src/main/java/com/example/flowpaywallet/dto/response/CreateUserResponse.java
@@ -7,4 +7,5 @@ import lombok.Setter;
 @Getter
 public class CreateUserResponse {
     private String message;
+    private String accountNumber;
 }
